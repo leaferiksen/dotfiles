2fa36166e8a7e9facf6ad9fef45b6df224933364
Leaf Eriksen <leaf@eriksens.org>
2024-07-07 20:19:02 -0700

    darkman config

---
+++ .local/share/dark-mode.d/dark.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+lookandfeeltool -platform offscreen --apply "org.kde.breezedark.desktop"
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
---
+++ .local/share/light-mode.d/light.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+lookandfeeltool -platform offscreen --apply "org.kde.breeze.desktop"
+gsettings set org.gnome.desktop.interface color-scheme prefer-light
